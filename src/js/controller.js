4e395198d45af08126c662ef95430594e293ec30
Pablo RDev <pabloriojod@gmail.com>
2022-09-16 02:00:22 +0200

    Normaliza files

---
+++ src/js/controller.js
@@ -0,0 +1,55 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import searchResultsView from "./views/searchResultsView.js";
+import paginationView from "./views/paginationView.js";
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const init = () => {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+
+const controlRecipes = async () => {
+  const id = window.location.hash.slice(1);
+  if (!id) throw new Error("No recipe ID found");
+
+  recipeView.renderSpinner();
+
+  try {
+    await model.loadRecipe(id);
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError();
+    console.error(`${error} 🤷‍♂️`);
+  }
+};
+
+const controlSearchResults = async () => {
+  searchResultsView.renderSpinner();
+
+  try {
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+    searchResultsView.render(model.getSearchResultPage());
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.error(`${error} 🤷‍♂️`);
+  }
+};
+
+const controlPagination = (page) => {
+  searchResultsView.render(model.getSearchResultPage(page));
+  paginationView.render(model.state.search);
+};
+
+init();
---
+++ src/js/model.js
@@ -0,0 +1,63 @@
+import { API_URL, RES_PER_PAGE } from "./config";
+import { getJson } from "./helpers";
+
+export const state = {
+  recipe: {},
+  search: {
+    query: "",
+    results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
+  },
+};
+
+export const loadRecipe = async (id) => {
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
+
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const loadSearchResults = async (query) => {
+  try {
+    state.search.query = query;
+
+    const data = await getJson(`${API_URL}?search=${query}`);
+
+    state.search.results = data.data.recipes.map((recipe) => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        publisher: recipe.publisher,
+        image: recipe.image_url,
+        servings: recipe.servings,
+        cookingTime: recipe.cooking_time,
+        ingredients: recipe.ingredients,
+      };
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getSearchResultPage = (page = state.search.page) => {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+};
